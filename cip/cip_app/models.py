28de1a43cf36c144fa81fd9469bd701b7bdd3231
robert-oppenheimer <ulusoyoguzhan@gmail.com>
2020-02-17 22:15:10 +0300

    commit

--- cip/cip_app/models.py
+++ cip/cip_app/models.py
@@ -8,6 +8,9 @@ from .lists import *
 
 class Person(models.Model):
 
+    # Proxy design pattern is applied by Django model.
+    Proxy = True
+
     # This is a bound between Person and User model
     # that is inherited from abstract Django user model.
     # It is never null or blank.
@@ -31,16 +34,43 @@ class Person(models.Model):
     # This is a list that includes blood types.
     blood_types = models.CharField(verbose_name='Blood types', unique=False, blank=False, null=False, help_text='Blood types', max_length=6, choices=SEX, default=SEX[0])
 
-    # This is a field that store address within maximum 100 characters.
+    # This is a field that stores address within maximum 100 characters.
     address = models.CharField(verbose_name='Address', unique=False, blank=True, null=True, help_text='Max 100 chars.', max_length=100)
 
+    # @important: Choice must be redefined.
+    # This is a field that stores town as free text.
     town = models.CharField(verbose_name='Town', unique=False, blank=True, null=True, help_text='Max 100 chars.', max_length=100)
 
-    city = models.CharField(verbose_name='City', unique=False, blank=True, null=True, help_text='Max 100 chars.', max_length=100)
+    # @important: Choice must be redefined.
+    # This is a list that includes cities in Turkey.
+    city = models.CharField(verbose_name='City', unique=False, blank=True, null=True, help_text='Select from list.', max_length=100, choices=SEX, default=SEX[0])
+
+    # This is date field for entrance to social security.
+    date_for_social_security = models.DateField(verbose_name='D4SS', unique=False, blank=False, null=False, help_text='Date for social security', auto_now=False)
+
+    # This is a field that stores mobile phone within prefix.
+    mobile_phone = models.CharField(verbose_name='Mobile phone', unique=False, blank=True, null=True, help_text='05373119592', max_length=11)
+
+    # This is a field that stores militaryship status just for men.
+    militaryship = models.BooleanField(verbose_name='Military status', unique=False, blank=True, null=True, help_text='Just for male', default=False)
 
 
+    '''
+    Aile bilgileri yer alacak
+    
+    Eğitim bilgileri yer alacak
+    
+    Acil durum bilgileri yer alacak
+    
+    Yabancı dil bilgileri yer alacak
 
+    '''
 
+    class Meta:
+        verbose_name = 'Person'
+        verbose_name_plural = 'People'
 
+    def __str__(self):
+        return str(self.identifier)
 
 
